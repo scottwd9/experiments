16098e10e876149be3ce6c4cf1ccaddb04a1b628
William Scott <wds@williamscott.me>
2015-01-28 22:30:10 -0500

    Round prices.

--- trailingStop.js
+++ trailingStop.js
@@ -1,7 +1,9 @@
 function trailingStop(price, stop_percent) {
-  var stop = price - price * stop_percent/100
+  var stop = price - price * stop_percent/100;
+  stop = Math.round(stop * 100) / 100
+  var limit = stop - 0.25
   return {
     stop: stop,
-    limit: stop - 0.25
+    limit: limit
   }
 }
